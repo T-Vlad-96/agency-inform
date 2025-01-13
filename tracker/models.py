15becc42850a3c02c1efcb33dc7fd87bdd7da23e
user <user@MacBook-Air-user.local>
2025-01-13 10:31:47 +0200

    the model 'Redactor' was created as a subclass of AbsctractUser with field 'years_of_experience'

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -8,6 +9,15 @@ class Topic(models.Model):
         return self.name
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
 
+
+class Redactor(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ["username"]
 
