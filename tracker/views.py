414dc0ce844ce918a60006615c593388b9f8e8b9
user <user@MacBook-Air-user.local>
2025-01-15 14:16:58 +0200

    url 'index', view index and template 'index.html' were added

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from tracker.models import Topic, Redactor, Newspaper
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_topics = Topic.objects.all().count()
+    num_redactors = Redactor.objects.all().count()
+    num_newspapers = Newspaper.objects.all().count()
+    num_visits = request.session.get("num_visits", 0)
+    num_visits += 1
+    request.session["num_visits"] = num_visits
+    context = {
+        "num_topics": num_topics,
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_visits": num_visits,
+    }
+    return render(request, "tracker/index.html", context=context)
