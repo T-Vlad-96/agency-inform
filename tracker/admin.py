3cbd4ca57bd5221ba73babcdfbaf7ed914e95c46
user <user@MacBook-Air-user.local>
2025-01-15 14:47:21 +0200

    Topic model was registrated in admin panel as a subclass of ModelAdmin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tracker.models import (
+    Topic,
+    Redactor,
+    Newspaper,
+)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
