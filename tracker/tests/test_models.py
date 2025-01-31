25b8a06d665042faa2103a72f0cd8f9c165faa84
user <user@MacBook-Air-user.local>
2025-01-31 11:12:42 +0200

    To the class RedactorModelTest were added methods 'test_redactor_str', 'test_redactor_ordering' and 'test_redactor_years_or_experience'

--- tracker/tests/test_models.py
+++ tracker/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
@@ -22,7 +23,26 @@ class TopicModelTest(TestCase):
 
 
 class RedactorModelTest(TestCase):
-    pass
+
+    def test_redactor_str(self):
+        redactor = Redactor(username="test")
+        self.assertEqual(str(redactor), "test")
+
+    def test_redactor_ordering(self):
+        self.assertEqual(Redactor._meta.ordering, ["username"])
+
+    def test_redactor_years_of_experience(self):
+        username = "test"
+        password = "Abc12345"
+        years_of_experience = 20
+        self.redactor = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            years_of_experience=years_of_experience,
+        )
+        self.assertEqual(self.redactor.username, username)
+        self.assertTrue(self.redactor.check_password(password))
+        self.assertEqual(self.redactor.years_of_experience, years_of_experience)
 
 
 class NewspaperModelTest(TestCase):
